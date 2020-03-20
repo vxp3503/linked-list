12b51f3bc7d606d55d717eb7b70a3ad4291871ea
vxp3503 <55551399+vxp3503@users.noreply.github.com>
2020-03-20 18:35:57 +0530

    Update linked list.cpp

--- linked list.cpp
+++ linked list.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
-
+#include<stdlib.h>
 using namespace std;
 struct node
 {
 	int data;
 	struct node* next;
-	}*temp,*newnode,*prev;
+	}*temp,*newnode,*prev,*opes;
 	
 	struct node* head = NULL;
     	 createnode()
@@ -22,24 +22,81 @@ struct node
 			temp->next=newnode;
 			temp=temp->next;
 		}}
-		display(struct node* x)
+		display(struct node* x,int count)
 		{
+			
 			while(x!=NULL)
 			{
 				cout<<x->data;
 				x=x->next;
+				count++;
+			}
+			if(count==1)
+			{
+				temp=head;
+			}
+			
+		}
+		deletenode(int n)
+		{
+			int i=1,d;
+			cout<<"enter the node which you want to delete";
+			cin>>d;
+			opes=head;
+			if(d==1)
+			{
+				head=opes->next;
+			    delete opes;
+			}
+			else if(d==n)
+			{
+				while(opes->next!=NULL)
+				{
+					prev=opes;
+					opes=opes->next;
+				}
+				prev->next=NULL;
+				delete opes;
+			}
+			else
+			{
+				while(i<d)
+				{
+					prev=opes;
+					opes=opes->next;
+					i++;
+				}
+				prev->next=opes->next;
+				delete opes;
 			}
 		}
 	
 	int main()
 	{
-		int i,n;
-		cout<<"enter no. of nodes";
-		cin>>n;
-		for(i=0;i<n;i++)
+		int i,n,c=0;
+		cout<<"here we can see list is empty";
+		display(head,c);
+		cout<<endl;
+		while(1)
 		{
-			createnode();
-		}
-		display(head);
-		return 0;
-	}
+		    cout<<"enter what do u want to do ";
+		    cin>>n;
+		    switch(n)
+		     {
+		        case 1:
+		        createnode();
+		       	display(head,c);
+		       	break;
+	       	    case 2:
+			    deletenode(c);
+			    display(head,c);
+			    break;
+			    case 3:
+			    	exit (0);
+			    	break;
+	           	default:
+		    	cout<<"operation successful on linked list";
+	        }
+	    }	   
+	}				
+			
